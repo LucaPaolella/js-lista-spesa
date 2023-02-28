009a3efce679fefe053fac9541dc64600b233f91
Your Name <you@example.com>
2023-02-28 15:39:53 +0100

    fine?

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
 "use strict";
+
+const ul =document.getElementById("shopping-list");
+
+const shoppingList = [
+    "Pane",
+    "Vino",
+    "Latte",
+    "Verdure"
+]
+    console.log(ul)
+
+    /*VERSIONE COL FOR
+    
+    for(let i =0; i< shoppingList.length; i++){
+        console.log(shoppingList[i]);
+        const listItem = document.createElement('li');
+        listItem.append(shoppingList[i]);
+        ul.append(listItem);
+
+    }*/
+
+    /*VERSIONE CON WHILE*/
+
+let i = 0;
+
+while (i < shoppingList.length) {
+    console.log(shoppingList[i]);
+    
+
+    const listItem = document.createElement('li');
+    listItem.append(shoppingList[i]);
+    ul.append(listItem);
+
+    i++;
+}
